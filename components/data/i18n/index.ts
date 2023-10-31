f5316e06672c669b9c894ac8046e9e19786e8582
helua <helua@users.noreply.github.com>
2023-10-31 23:21:08 +0100

    couple of changes to translation PL

--- components/data/i18n/index.ts
+++ components/data/i18n/index.ts
@@ -2,7 +2,7 @@ import i18n, { use } from "i18next"
 import translationDE from "public/static/locales/de/common.json"
 import translationEN from "public/static/locales/en/common.json"
 import translationIT from "public/static/locales/it/common.json"
-import translationPL from "public/static/locales/it/common.json"
+import translationPL from "public/static/locales/pl/common.json"
 import { initReactI18next } from "react-i18next"
 
 const languages = ["en", "it", "de", "pl"]
